1abe9bd06b6b25ab8d6ad89b581f929180832d2c
iwyoo <doveu1234@gmail.com>
2017-01-30 16:49:43 +0900

    another version

---
+++ test2.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from ThinPlateSpline2 import ThinPlateSpline2 as stn
+
+img = np.array(Image.open("original.png"))
+out_size = list(img.shape)
+shape = [1]+out_size+[1]
+
+s_ = np.array([
+  [-0.5, -0.5],
+  [0.5, -0.5],
+  [-0.5, 0.5],
+  [0.5, 0.5]])
+
+t_ = np.array([
+  [-0.6, -0.6],
+  [0.6, 0.6],
+  [-0.6, 0.6],
+  [0.6, 0.6]])
+
+s = tf.constant(s_.reshape([1, 4, 2]), dtype=tf.float32)
+t = tf.constant(t_.reshape([1, 4, 2]), dtype=tf.float32)
+t_img = tf.constant(img.reshape(shape), dtype=tf.float32)
+t_img = stn(t_img, s, t, out_size)
+
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  img1 = sess.run(t_img)
+  Image.fromarray(np.uint8(img1.reshape(out_size))).save("transformed2.png")
